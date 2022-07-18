179b066fac3e5951a6ed88f9b4efd93329067c0d
Arina_Nichko <oryna_nichko@epam.com>
2022-07-18 19:43:32 +0300

    first commit

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+
+    @Test
+    public void getProduct(){
+        String endpoint = "http://localhost:3306/api_testing/product/read_one.php";
+        given().
+                queryParam("id", 2).
+                when().
+                get(endpoint).
+                then().
+                assertThat().
+                statusCode(200).
+                body("id", equalTo("2")).
+                body("name", equalTo("Cross-Back Training Tank")).
+                body("description", equalTo("The most awesome phone of 2013!")).
+                body("price", equalTo("299.00")).
+                body("category_id", equalTo("2")).
+                body("category_name", equalTo("Active Wear - Women"));
+    }
+
+    @Test
+    public void createProduct(){
+        String endpoint = "http://localhost:3306/api_testing/product/create.php";
+        String body = """
+                {
+                "name": "Water Bottle",
+                "description": "Blue water bottle. Holds 64 ounces",
+                "price": 12,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().post(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void updateProduct(){
+        String endpoint = "http://localhost:3306/api_testing/product/update.php";
+        String body = """
+                {
+                "id": 19,
+                "name": "Water Bottle",
+                "description": "Blue water bottle. Holds 64 ounces",
+                "price": 15,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().put(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void deleteProduct(){
+        String endpoint = "http://localhost:3306/api_testing/product/delete.php";
+        String body = """
+                {
+                "id": 19
+                }
+                """;
+        var response = given().body(body).when().delete(endpoint).then();
+        response.log().body();
+    }
+
+}
