aaa54e3faac2d6d6c7a98a8e0cc070f133d935d2
Arina_Nichko <oryna_nichko@epam.com>
2022-08-23 20:06:02 +0300

    Project was improved

--- src/main/java/employee/configPath/ConfigurationPath.java
+++ src/main/java/employee/configPath/ConfigurationPath.java
@@ -1,6 +1,6 @@
-package config;
+package employee.configPath;
 
-public class Path {
+public class ConfigurationPath {
     public static final String URL = "http://localhost:9090/";
     public static final String getEmployee = "/102";
     public static final String deleteNegativeRequest = "/105";
@@ -15,8 +15,12 @@ public class Path {
     public static String pathNegativeRequest = "/request_negative.json";
     public static String pathPost = "/post_employee";
     public static String pathGet = "/get_employee";
-    public static String responsePath= "message";
-    public static  int SUCCESS_CODE = 200;
-    public static  int INTERNAL_SERVER_ERROR = 500;
+    public static String responsePath = "message";
+    public static int SUCCESS_CODE = 200;
+    public static int INTERNAL_SERVER_ERROR = 500;
+
+    public String fullPathPositiveResponse(String httpMethod) {
+        return pathTemplate + httpMethod + pathPositiveResponse;
+    }
 
 }
--- src/main/java/employee/model/Employee.java
+++ src/main/java/employee/model/Employee.java
@@ -1,4 +1,4 @@
-package clients;
+package employee.model;
 
 
 import lombok.AllArgsConstructor;
